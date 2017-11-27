5261c5f2d4594aeb2882f3c438bae4d16fbcb3bd
Akash Bhavsar <abh@odoo.com>
2017-11-27 13:04:39 +0530

    [ADD] Added C_CRM module

---
+++ addons/C_CRM/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'C-CRM',
+    'version': '1.0',
+    'depends': [],
+    'description': "",
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
+
